ac3efd2fd2ee19900545d8fc1a5e5625a3a74c3f
AbdelrahmanNasser00 <Abdo2652149@gmail.com>
2021-11-02 01:12:28 +0200

    Added the files

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<cmath>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <numeric>
+using namespace std;
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int x, y;
+	cin >> x >> y;
+	cout << x + y<<endl;
+	system("pause");
+}
